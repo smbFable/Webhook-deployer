e60748a11cfe561bf9d49687561df0d2012cbaa9
smbFable <arseniyburduk@gmail.com>
2026-07-13 21:25:21 +0300

    test_3

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/PWS", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Привет, localhost!")
-	})
+	http.HandleFunc("/PWS", JSONProcessing)
 
 	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
 		return
 	}
+}
+
+func JSONProcessing(w http.ResponseWriter, r *http.Request) {
+	r.Header.Add("Content-Type", "application/json")
+	fmt.Fprintln(w, "Hello, World!")
 }
