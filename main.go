978ff92675cd00bd43dac8d04bf71091d7841570
smbFable <arseniyburduk@gmail.com>
2026-07-16 15:25:14 +0300

    test_16

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -20,5 +23,22 @@ func JSONProcessing(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		fmt.Println("Неверный метод")
 	}
-	
+	_, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(w, "Ошибка чтения тела запроса: ", err)
+		return
+	}
+	defer r.Body.Close()
+
+	sheader := r.Header.Get("X-Hub-Signature-256")
+	if sheader == "" {
+		fmt.Println("Заголовок с secret пуст")
+		return
+	}
+
+	WHSecret := []byte("smbFableSecret1")
+
+	mac := hmac.New(sha256.New, WHSecret)
+	fmt.Println(mac)
+
 }
